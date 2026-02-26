5bbd3914d773acb170acf92d5a1872fed7da990d
Weverton Martins <wevertonm22@icloud.com>
2026-02-26 19:26:07 -0300

    Arrumando o bug do loop infinito

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-     if (de >= ate) {
+    if (de >= ate) {
       alert(`Campo ${de} dever se inferior o Campo ${ate}`);
       return;
     }
-
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+    return;
+    }
     let sorteados = [];
     let numero;
 
