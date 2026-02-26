f8a6437a5f957fc1905697ce75623d8c1605a53f
Weverton Martins <wevertonm22@icloud.com>
2026-02-26 16:24:44 -0300

    colocando parseInt

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
     
     alert(`Quantidade: ${quantidade}`);
     alert(`Do número: ${de}`);
     alert(`Até o número: ${ate}`);
-
-
 }
